71a619bead4ac14f19317cdc50866953d82e59df
lap1zl4zuli <hychhil@gmail.com>
2024-07-24 18:57:19 +0530

    chore: Update display_video function to use courses_dict instead of course_dict

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(
 st.session_state['User courses'] = {}
 
 
-def display_video(course_dict):
+def display_video(courses_dict):
     for key in courses_dict.keys():
         st.write(f'### {key}')
         for i, video in enumerate(courses_dict[key]['videos']):
@@ -28,16 +28,27 @@ def display_video(course_dict):
                 key=f"{key}_{i}",
                 styles={
                     'card': {
-                        'width': '560px',
-                        'height': '360px',
+                        'width': vid['thumbnails']['standard']['width'],
+                        'height': vid['thumbnails']['standard']['height'],
+                        'margin': '5px',
                     },
                 }
             )
+    st.download_button(
+        label="Download",
+        data=json.dumps(courses_dict),
+        file_name=f"course_{query}.json",)
 
-st.title('👋🏻Hello!')
-query = st.text_input('Course Topic')
-subtopics = st.number_input(
-    'No. of subtopics', step=1, max_value=10, min_value=0)
+
+st.title('YouTube Course Recommender')
+st.write('This app recommends a YouTube course based on the topic and number of subtopics you provide.')
+cols = st.columns([4, 1])
+with cols[0]:
+    query = st.text_input(
+        'Course Topic', placeholder='Enter the topic for the course', label_visibility='hidden')
+with cols[1]:
+    subtopics = st.number_input(
+        'No. of subtopics', step=1, max_value=10, min_value=0, help='Enter the number of subtopics',)
 create = st.button('Create Course')
 if create:
     try:
@@ -45,6 +56,8 @@ if create:
         st.session_state['User courses'][query] = agent.recommend(
             query, subtopics)
         courses_dict = st.session_state['User courses'][query]
+        # with open('rm.json', encoding='utf-8') as f:
+        #     courses_dict = json.load(f)
         display_video(courses_dict)
     except Exception as e:
-        st.exception(e)
+        st.error(e, icon='⚠️')
